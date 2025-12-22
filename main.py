8bf69ddec39b70ddde91bbf1fbf4d6d1894a1955
Jonathan Klaric <jon.klaric@gmail.com>
2025-12-22 17:25:00 +1000

    Added main game loop.

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+import constants
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    # Screen width: 1280
+    # Screen height: 720
+    while True:
+        log_state()
+        # check for closing the window...
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                break
+        screen.fill("black")
+        # refresh the screen
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
