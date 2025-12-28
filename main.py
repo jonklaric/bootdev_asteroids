874d7305940569f75589c4fc9993e4850f73fb32
Jonathan Klaric <jon.klaric@gmail.com>
2025-12-28 10:00:01 +1000

    Added player to main game loop.

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import pygame
 import constants
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-
+from player import Player
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     # Screen width: 1280
     # Screen height: 720
     while True:
@@ -19,10 +23,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # set background to be black.
         screen.fill("black")
-        # refresh the screen
-        pygame.display.flip()
 
+        # draw player
+        player.draw(screen)
+        player.update(dt)
+
+        ### refresh the screen
+        pygame.display.flip()
+        dt = game_clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
